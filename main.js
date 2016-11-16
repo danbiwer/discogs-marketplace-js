57dc3531f353a54a21434af474cc8032a622f8d8
Daniel <danbiwer@gmail.com>
2016-11-16 11:55:40 -0600

    refactoring generateResult function

--- main.js
+++ main.js
@@ -8,11 +8,23 @@ var cheerio = require('cheerio');
 
 var path = "https://www.discogs.com/sell/release/1067610";
 var path = "https://www.discogs.com/sell/list?master_id=655666&ev=mb";
+var path = "https://www.discogs.com/sell/list?artist_id=39765&ev=ab";
 
-function generateUrl(){
+//type (optional) can be master, release, label, everything, id.  Defaults to id.
+function searchByID(id, type, filters){
+	var path = "https://www.discogs.com/sell/list?q=";
+	if(typeof type === "undefined"){
 
+	}
+	path = path + id;
 }
 
+//type (optional) can be search, id. Default to id
+function searchByString(id, filters){
+	var path = "https://www.discogs.com/sell/list?q=";
+}
+
+//todo: handle paging
 function generateResult(path){
 	request(path, function(error, response, body){
 		var result = new Object();
@@ -26,14 +38,7 @@ function generateResult(path){
 		statistics.avg_rating = $("#statistics").find("li .rating_value").text();
 		statistics.rating_num = $("#statistics").find("li .rating_count").text();
 		$(".shortcut_navigable").each(function(i, obj){
-			var item = new Object();
-			item.title = $(obj).find(".item_description_title").text();
-			item.condition_sleeve = $(obj).find(".item_sleeve_condition").text();
-			item.condition_media = $(obj).find(".item_condition span").eq(2).text().trim();
-			item.seller = $(obj).find(".seller_info a").eq(0).text();
-			item.ships_from = $(obj).find(".seller_info li").eq(2).text().replace("Ships From:","");
-			item.price = $(obj).find(".price").eq(0).text();
-			//seller info - more info needed?
+			var item = processItem(obj);
 			listing.push(item);
 		});
 		result.statistics = statistics;
@@ -41,6 +46,18 @@ function generateResult(path){
 		console.log(result);
 		//return result;
 	});
+}
+
+function processItem(obj){
+	var $ = cheerio.load(obj);
+	var item = new Object();
+	item.title = $(obj).find(".item_description_title").text();
+	item.condition_sleeve = $(obj).find(".item_sleeve_condition").text();
+	item.condition_media = $(obj).find(".item_condition span").eq(2).text().trim();
+	item.seller = $(obj).find(".seller_info a").eq(0).text();
+	item.ships_from = $(obj).find(".seller_info li").eq(2).text().replace("Ships From:","");
+	item.price = $(obj).find(".price").eq(0).text();
+	return item;
 }
 //look up how a promise is used
 generateResult(path);
