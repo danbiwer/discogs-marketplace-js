7e0af45f6f89fb963059850c200dc38fe714dc49
Daniel <danbiwer@gmail.com>
2016-12-01 21:59:24 -0600

    exposed search functionality

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+Discogs Marketplace API
+Author: Daniel Biwer (danbiwer@gmail.com)
+*/
+
+'use strict';
+
+var DiscogsMarketplaceAPI = module.exports = {};
+
+DiscogsMarketplaceAPI = require('./lib/search.js');
